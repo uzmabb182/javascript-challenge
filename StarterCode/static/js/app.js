0b73e81fac4e180a8a22e11d173b094735cf520f
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-12 15:49:09 -0400

    bubble chart is added in initial page load

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,9 +2,14 @@
 This is not the only way to complete this assignment.
 Feel free to disregard and create your own code */
 
-let selectDropdown = d3.select("#selDataset");
+let arrowDropdown = d3.select("#selDataset");
 
-
+arrowDropdown.on("change", function(){
+let newText = d3.event.target.value;
+console.log(newText);
+console.log(arrowDropdown.text())
+    
+});
 // Define function that will run on page load
 function init() {
 
@@ -20,18 +25,19 @@ function init() {
         console.log(firstId);
         
         data.names.forEach((name, i) => {
-            let appendOption = selectDropdown.append("option").attr('value', i).text(name);
+            let appendOption = arrowDropdown.append("option").text(name).attr('value', i);
         });
       
     });
     
-
     // Call functions below using the first sample to build metadata and initial plots
-
+    buildMetadata();
+    buildCharts();
 }
 
 // Define a function that will create metadata for given sample
 function buildMetadata() {
+
     let demographicInfoBox = d3.select("#sample-metadata");
     // Read the json data
     // Parse and filter the data to get the sample's metadata
@@ -60,21 +66,61 @@ function buildMetadata() {
 // Define a function that will create charts for given sample
 function buildCharts(sample) {
 
+    let bubbleChart = d3.select("#bubble");
+
     // Read the json data
+
+    d3.json("samples.json").then(function(data) {
 
         // Parse and filter the data to get the sample's OTU data
         // Pay attention to what data is required for each chart
-
+        let samplesData = data.samples;
+        console.log(samplesData);
+        // fitering first matching item from' metadata' which is again an object 
+        filterData = samplesData.filter(firstItem => firstItem.id == data.names[0])
+        console.log(filterData)
+        // assigning the first item in the object to a variable
+        filterData = filterData[0]
+        console.log(filterData.id)
+        Id = filterData.id
+        otuIds = filterData.otu_ids
+        sampleValues = filterData.sample_values
+        otuLabels = filterData.otu_labels
+        console.log(Id)
+        console.log(otuIds)
+        console.log(sampleValues)
+        console.log(otuLabels)
         // Create bar chart in correct location
-
+        var trace1 = {
+            x: otuIds,
+            y: sampleValues,
+            mode: 'markers',
+            marker: {
+              color: otuIds,
+              opacity: [1, 0.8, 0.6, 0.4],
+              size: sampleValues
+            }
+          };
+          
+          var data = [trace1];
+          
+          var layout = {
+            title: 'otu_ids Vs. sample_values',
+            showlegend: false,
+            height: 500,
+            width: 1300
+          };
+          
+          Plotly.newPlot('bubble', data, layout);
         // Create bubble chart in correct location
+    });
     
 }
 
 
 function optionChanged(sample){
     // The parameter being passed in this function is new sample id from dropdown menu
-
+   
     // Update metadata with newly selected sample
 
     // Update charts with newly selected sample
@@ -83,5 +129,5 @@ function optionChanged(sample){
 
 // Initialize dashboard on page load
 init();
-buildMetadata();
+
 
