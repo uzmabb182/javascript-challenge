6e11c9924e3d8956996b47d8341e62fae0ee441e
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-12 11:45:09 -0400

    demographic info added on page load

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,18 +4,25 @@ Feel free to disregard and create your own code */
 
 let selectDropdown = d3.select("#selDataset");
 
+
 // Define function that will run on page load
 function init() {
 
     // Read json data
     // Parse and filter data to get sample names 
     // Add dropdown option for each sample
-
+    // data is an object with three arrays, names, metadat, and samples
     d3.json("samples.json").then(function(data) {
-
+        console.log(data);
+        // fetching first id from 'names' which is an array of items
+        // console.log(data.names[0]);
+        let firstId = data.names[0];
+        console.log(firstId);
+        
         data.names.forEach((name, i) => {
             let appendOption = selectDropdown.append("option").attr('value', i).text(name);
         });
+      
     });
     
 
@@ -24,13 +31,29 @@ function init() {
 }
 
 // Define a function that will create metadata for given sample
-function buildMetadata(sample) {
-
+function buildMetadata() {
+    let demographicInfoBox = d3.select("#sample-metadata");
     // Read the json data
+    // Parse and filter the data to get the sample's metadata
+    // Specify the location of the metadata and update it
+    d3.json("samples.json").then(function(data) {
 
-        // Parse and filter the data to get the sample's metadata
+       let metaData = data.metadata;
+       //console.log(data.metadata[1]);
+       console.log(metaData);
+      
+       // fitering first matching item from' metadata' which is again an object 
+        filterData = metaData.filter(firstItem => firstItem.id == data.names[0])
+        console.log(filterData)
+        // assigning the first item in the object to a variable
+        filterData = filterData[0]
+        console.log(filterData.id)
 
-        // Specify the location of the metadata and update it
+        Object.entries(filterData).forEach(([key, value]) => {
+            demographicInfoBox.append("h6").text(`${key} => ${value}`)
+        });
+    });
+
 
 }
 
@@ -60,4 +83,5 @@ function optionChanged(sample){
 
 // Initialize dashboard on page load
 init();
+buildMetadata();
 
