c44321aceb92b196413d165082da70b2cd0c831a
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-13 13:28:55 -0400

    bar chart fixed and gauge chart updated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,42 +7,44 @@ Feel free to disregard and create your own code */
 
 // Define function that will run on page load
 function init() {
-    
+
     // Read json data
     // Parse and filter data to get sample names 
     // Add dropdown option for each sample
     // data is an object with three arrays, names, metadat, and samples
     let arrowDropdown = d3.select("#selDataset");
 
-    d3.json("samples.json").then(function(data) {
+    d3.json("samples.json").then(function (data) {
         console.log(data);
+        console.log(data.names[0]);
         // fetching first id from 'names' which is an array of items
         data.names.forEach((name, i) => {
             let appendOption = arrowDropdown.append("option").text(name).attr('value', name);
         });
-      
+        // Call functions below using the first sample to build metadata and initial plots
+        buildMetadata(data.names[0]);
+        buildCharts(data.names[0]);
     });
-    
-    // Call functions below using the first sample to build metadata and initial plots
-    buildMetadata();
-    buildCharts();
+
+
 }
 
 // Define a function that will create metadata for given sample
 function buildMetadata(sample) {
     console.log(sample)
     let demographicInfoBox = d3.select("#sample-metadata");
+    demographicInfoBox.html("")
     // Read the json data
     // Parse and filter the data to get the sample's metadata
     // Specify the location of the metadata and update it
-    d3.json("samples.json").then(function(data) {
+    d3.json("samples.json").then(function (data) {
 
         let metaData = data.metadata;
         //console.log(data.metadata[1]);
         console.log(metaData);
-       
+
         // fitering first matching item from' metadata' which is again an object 
-        filterData = metaData.filter(firstItem => firstItem.id == data.names[0])
+        filterData = metaData.filter(firstItem => firstItem.id == sample)
         console.log(filterData)
         // assigning the first item in the object to a variable
         filterData = filterData[0]
@@ -63,14 +65,14 @@ function buildCharts(sample) {
 
     // Read the json data
 
-    d3.json("samples.json").then(function(data) {
-        
+    d3.json("samples.json").then(function (data) {
+
         let metaData = data.metadata;
         //console.log(data.metadata[1]);
         console.log(metaData);
-      
+
         // fitering first matching item from' metadata' which is again an object 
-        filtermetaData = metaData.filter(firstItem => firstItem.id == data.names[0])
+        filtermetaData = metaData.filter(firstItem => firstItem.id == sample)
         console.log(filtermetaData)
         // assigning the first item in the object to a variable
         filtermetaData = filtermetaData[0]
@@ -81,7 +83,7 @@ function buildCharts(sample) {
         let samplesData = data.samples;
         console.log(samplesData);
         // fitering first matching item from' metadata' which is again an object 
-        filterData = samplesData.filter(firstItem => firstItem.id == data.names[0])
+        filterData = samplesData.filter(firstItem => firstItem.id == sample)
         console.log(filterData)
         // assigning the first item in the object to a variable
         filterData = filterData[0]
@@ -101,22 +103,22 @@ function buildCharts(sample) {
             y: sampleValues,
             mode: 'markers',
             marker: {
-              color: otuIds,
-              opacity: [1, 0.8, 0.6, 0.4],
-              size: sampleValues
+                color: otuIds,
+                opacity: [1, 0.8, 0.6, 0.4],
+                size: sampleValues
             }
-          };
-          
-          var data = [trace1];
-          
-          var layout = {
+        };
+
+        var data = [trace1];
+
+        var layout = {
             title: 'otu_ids Vs. sample_values',
             showlegend: false,
             height: 500,
             width: 1300
-          };
-          
-          Plotly.newPlot('bubble', data, layout);
+        };
+
+        Plotly.newPlot('bubble', data, layout);
 
         // Create bar chart in correct location
         // Slice the first ten names
@@ -124,14 +126,14 @@ function buildCharts(sample) {
         let otuIdsten = otuIds.slice(0, 10);
         console.log(otuIdsten)
         var trace1 = {
-            x: sampleValues,
-            y: otuIdsten,
-           
+            x: sampleValues.slice(0, 10).reverse(),
+            y: otuIdsten.map(otuId => `ID #${otuId}`).reverse(),
+
             marker: {
                 // color: ['rgba(204,204,204,1)', 'rgba(222,45,38,0.8)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)']
             },
             type: 'bar',
-            orientation:"h"
+            orientation: "h"
 
         };
 
@@ -139,39 +141,40 @@ function buildCharts(sample) {
 
         var layout = {
             title: 'Top Ten OTUs Found',
-           
+
         };
 
         Plotly.newPlot('bar', data, layout);
 
         // gauge chart
-       
+
 
         var data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
                 value: filtermetaData.wfreq,
-                title: { text: "Speed" },
+                title: { text: "Belly Button Washing Freq." },
                 type: "indicator",
                 mode: "gauge+number"
             }
         ];
-        
+
         var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
         Plotly.newPlot('gauge', data, layout);
 
 
 
     });
-    
+
 }
 
 
-function optionChanged(sample){
+function optionChanged(sample) {
     // The parameter being passed in this function is new sample id from dropdown menu
-   console.log(sample)
+    console.log(sample)
     // Update metadata with newly selected sample
-
+    // buildMetadata(sample);
+    // buildCharts(sample);
     // Update charts with newly selected sample
 
 }
